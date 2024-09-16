40b7d73bebfd66cdaa454d2268fab027f1328e06
Prodyun Biswas <92980967+prodyun08@users.noreply.github.com>
2024-09-16 23:17:07 +0530

    Add

---
+++ Week 1/Day 1/Redme.md
@@ -0,0 +1,6 @@
+#Improving Your First Python Program
+We can personalize your first Python program.
+In our text editor in hello.py we can add another function. input is a function that takes a prompt as an argument. We can edit our code to say
+
+"""input("What's your name? ")
+print("hello, world")"""
--- Week 1/Day 1/hello.py
+++ Week 1/Day 1/hello.py
@@ -1,1 +1,24 @@
-print("Hello, World!")
+#first Program
+"""
+input("What's your name? ")
+print("hello, world")
+"""
+
+#Variables
+"""
+name = input("What's your name? ")
+print("hello, world")
+
+name = input("What's your name? ")
+print("hello, name")
+
+name = input("What's your name? ")
+print("hello,")
+print(name)
+
+What's your name? Prodyun
+hello
+Prodyun
+"""
+
+
